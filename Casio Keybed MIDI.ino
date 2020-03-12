86e5a516182b683c29e549da0a9477036780f317
gjttt1 <dannyherbertgj@gmail.com>
2020-03-12 16:52:12 +0000

    First working revision

---
+++ Casio Keybed MIDI.ino
@@ -0,0 +1,112 @@
+#include <MIDIUSB.h>
+
+const byte NUMK    = 88;
+const byte NUMI    = 11;
+const byte NUMO    = 8;
+const byte CHANNEL = 1;
+const byte OFFSET = 9+24;
+const byte inputPins[NUMI]    = {22, 24, 26, 28, 30, 41, 43, 45, 47, 49, 51};
+const byte inputPinsVel[NUMI] = {23, 25, 27, 29, 31, 40, 42, 44, 46, 48, 50};
+const byte outputPins[NUMO]   = {32, 33, 34, 35, 36, 37, 38, 39};
+
+bool noteOn[NUMK]             = {};
+bool noteOnPrev[NUMK]         = {};
+unsigned long noteVel[NUMK]   = {};
+unsigned long noteTimer[NUMK] = {};
+
+void setup()
+{
+    for (int i = 0; i < NUMI; ++i)
+    {
+        pinMode(inputPins[i], INPUT_PULLUP);
+        pinMode(inputPinsVel[i], INPUT_PULLUP);
+    }
+    for (int i = 0; i < NUMO; ++i)
+    {
+        pinMode(outputPins[i], OUTPUT);
+    }
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    updateNoteArray();
+    sendNotes();
+}
+
+
+void updateNoteArray()
+{
+    for (byte i = 0; i < NUMO; ++i)
+    {
+        digitalWrite(outputPins[i], LOW ); 
+
+        for (byte j = 0; j < NUMI; ++j) 
+        {
+            byte currentNote = ((j + 1) * NUMO) - ((NUMO - 1) - i) - 1; 
+
+            //KEY START - key has began being pressed
+            if (digitalRead(inputPins[j]) == LOW) 
+            {
+                if (noteTimer[currentNote] == 0) 
+                {
+                    noteTimer[currentNote] = micros();  
+                }
+            }
+            else 
+            {
+                noteTimer[currentNote] = 0;
+                noteVel[currentNote] = 0;
+            }
+
+            if (digitalRead(inputPinsVel[j]) == LOW)
+            {
+                if (noteVel[currentNote] == 0)   
+                {
+                    noteVel[currentNote] = calcVel(micros() - noteTimer[currentNote]);
+                }  
+                if (noteOn[currentNote]  == false)
+                {
+                    noteOn[currentNote] = true;
+                } 
+            }
+            else
+            {
+                noteOn[currentNote] = false;
+            }
+        }
+
+        digitalWrite(outputPins[i], HIGH);
+    }
+}
+
+byte calcVel(unsigned long value)
+{
+    value = constrain(value, 5000, 100000);
+    value = map(value, 5000, 100000, 127, 1);
+    return (byte)value;
+}
+
+void sendNotes()
+{
+    for (int i = 0; i < NUMK; i++)
+    {
+        if (noteOn[i] != noteOnPrev[i])
+        {
+            byte note = i + OFFSET;
+            if (noteOn[i] == true) sendNoteOn (1, note, noteVel[i]);
+            else                   sendNoteOff(1, note, 0);
+            noteOnPrev[i] = noteOn[i];
+        }
+    }
+}
+
+void sendNoteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void sendNoteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
