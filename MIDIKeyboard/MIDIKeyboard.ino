42b00e90d86995a6ac2901b8c052ac6e363bcd79
DannyHerbert <dannyherbertgj@gmail.com>
2020-11-13 19:35:52 +0000

    Fixed sustain pedal

--- MIDIKeyboard/MIDIKeyboard.ino
+++ MIDIKeyboard/MIDIKeyboard.ino
@@ -35,8 +35,9 @@ void setup()
     //Sustain Pins
     pinMode(sustainPins[0], INPUT_PULLUP);
     pinMode(sustainPins[1], OUTPUT);
+    digitalWrite(sustainPins[1], LOW);
     
-    Serial.begin(9600);
+    //Serial.begin(9600);
 }
 
 void loop()
@@ -95,15 +96,8 @@ void updateNoteArray()
 
 void updateSustain()
 {
-  digitalWrite(sustainPins[0], LOW);
-  const bool sustainOn = digitalRead(sustainPins[1]);
-  if (sustainOn != sustainOnPrev)
-  {
-    
-    
-  }
-  digitalWrite(sustainPins[0], HIGH);
-  
+  sustainOn = digitalRead(sustainPins[0]);
+  //Serial.print(sustainOn);
 }
 
 byte calcVel(unsigned long value)
@@ -121,8 +115,8 @@ void sendMIDI()
         if (noteOn[i] != noteOnPrev[i])
         {
             byte note = i + OFFSET;
-            if (noteOn[i] == true) sendNoteOn (1, note, noteVel[i]);
-            else                   sendNoteOff(1, note, 0);
+            if (noteOn[i] == true) sendNoteOn (CHANNEL, note, noteVel[i]);
+            else                   sendNoteOff(CHANNEL, note, 0);
             noteOnPrev[i] = noteOn[i];
         }
     }
@@ -130,11 +124,11 @@ void sendMIDI()
     //Send Sustain Data
     if (sustainOn != sustainOnPrev)
     {
-      sendControlChange(1, SUSTAINCC, sustainOn * 120);
+      byte value = sustainOn ? 0 : 127;
+      sendControlChange(CHANNEL, SUSTAINCC, value);
       sustainOnPrev = sustainOn;
     }
-    
-    MidiUSB.flush();
+   
 }
 
 void sendControlChange(byte channel, byte control, byte value) {
@@ -142,16 +136,19 @@ void sendControlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
 
   MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
 }
 
 void sendNoteOn(byte channel, byte pitch, byte velocity) 
 {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
+  MidiUSB.flush();
 }
 
 void sendNoteOff(byte channel, byte pitch, byte velocity) 
 {
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOff);
+  MidiUSB.flush();
 }
